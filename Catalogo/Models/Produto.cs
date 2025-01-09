50ab4d4d788997a77303698ca60443f3879ddacf
KaylanDev <k1ndealblack@gmail.com>
2025-01-09 19:40:41 -0300

    perf: add data annotations com mensagens de erro

--- Catalogo/Models/Produto.cs
+++ Catalogo/Models/Produto.cs
@@ -10,20 +10,21 @@ public class Produto
     public int ProdutoId { get; set; }
 
     [Required]
-    [StringLength(80)]
+    [StringLength(80,MinimumLength = 5 ,ErrorMessage ="Digite um nome valido!")]
     public string? Nome { get; set; }
-    
+
     [Required]
-    [StringLength(300)]
+    [StringLength(300, MinimumLength = 10, ErrorMessage = "A descrição deve ter entre {2} a {1}!")]
     public string? Descricao { get; set; }
 
     [Required]
-    [Column(TypeName = "decimal(10,2)")]
+    [Range(1, 1000, ErrorMessage = "O valor deve ser entre {1} e {2}")]
     public decimal Preco { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(300, MinimumLength = 3, ErrorMessage = "A imagem precisa ter entre {1} e {2} caracteres!")]
     public string? ImagemUrl { get; set; }
+    [Range(0,10000,ErrorMessage ="O Valor precisa estar entre {1} e {2}!")]
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     public int CategoriaId { get; set; }
